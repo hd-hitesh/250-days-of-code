8f3258d140baa0b451fa9b7cbe6697d1d07959f8
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-10-30 22:27:40 +0530

    day 1

---
+++ code.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	int t; cin >> t;
+
+	while (t--)
+	{
+		solve();
+		cout << endl;
+	}
+	
+	return 0;
+}
+
+// ctrl +  alt + F   :  format the code
+// ctrl + shift + B  :  build with
+// ctrl + B          :  build
+// ctrl + X          :  cut the line
+// ctrl +

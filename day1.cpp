9a49ec5b1fe88db21f110d63fc2ce42a9d98b3f3
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-11-02 11:10:14 +0530

    Day 2

---
+++ day1.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	long long x;
+	cin >> x;
+	cout << 0 << " " << x << "\n";
+
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	int t; cin >> t;
+
+	while (t--)
+	{
+		solve();
+		cout << endl;
+	}
+	
+	return 0;
+}
+
+//https://www.codechef.com/LTIME89B/problems/ANDOR
+
+// ctrl +  alt + F   :  format the code
+// ctrl + shift + B  :  build with
+// ctrl + B          :  build
+// ctrl + X          :  cut the line
+// ctrl +

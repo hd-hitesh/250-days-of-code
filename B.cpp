65b8ddb2b4ef76b35222c714a03c138f7dc97d08
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-11-26 00:09:47 +0530

    c

---
+++ B.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define len(x) x.size()
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+// #define FOR(i,j,n) for(int i=j;i<n;i++)
+// #define FORR(i,j,n) for(int i=j;i>n;i--)
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORR(i,n) for(int i=j;i>=0;i--)
+#define all(v) v.begin(), v.end()
+#define endl "\n";
+#define tez_chal_bsdk                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef unordered_map<int, int> mp;
+typedef unordered_map<ll, ll> mpl;
+
+#define cint(a) int a; cin>>a;
+#define cin(a) ll a; cin>>a;
+#define cin2(a,b) ll a,b; cin>>a>>b;
+#define cin3(a,b,c) ll a,b,c; cin>>a>>b>>c;
+#define vin(v,n) for(ll i=0; i<n;i++) cin>>v[i];
+#define vout(v,n) for(ll i=0; i<n;i++) cout<<v[i]<<" "; cout<<endl;
+
+void solve()
+{
+
+	cin(n)ll a[n]; mp m;
+	int mn = INT_MAX;
+	FOR(i, n) {
+		cin >> a[i];
+		m[a[i]]++;
+		// if(mn>a[i])mn=a[i];
+	}
+	// cout << "MIN =____ " << mn << " ____\n";
+	bool flag = true;
+	for (auto i : m)
+	{
+		if (i.second == 1 && i.first < mn)
+		{
+			flag = false; mn = i.first;
+		}
+	}
+	if (flag) {cout << "-1"; return;}
+	FOR(i,n){
+		if(a[i]==mn)
+			{cout<<i+1;break;}
+	}
+}
+
+int main()
+{
+	tez_chal_bsdk;
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	// solve();
+	cint(t);
+	while (t--)
+	{
+		solve();
+		cout << endl;
+	}
+
+	return 0;
+}
+
+// ctrl +  alt + F   :  format the code
+// ctrl + shift + B  :  build with
+// ctrl + B          :  build
+// ctrl + X          :  cut the line
+// ctrl +
